77b79085e60844cc6eee899f1cf17af2fcf152ff
rossakyl <rossakayla01@gmail.com>
2025-06-10 11:41:44 +0700

    tambahkan pengecekan underflow saat antrian kosong

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -44,4 +44,13 @@ public:
         queue_array[REAR] = num;
     }
 
+    void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+    }
 };
