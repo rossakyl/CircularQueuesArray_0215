e4071049d2fbc659bfe884333dc4a975ff126f32
rossakyl <rossakayla01@gmail.com>
2025-06-10 11:51:57 +0700

    Menampilkan isi antrian saat antrian dalam kondisi melingkar (circular queue)

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -93,5 +93,23 @@ public:
             }
             cout << endl;
         }
+        else
+        {
+            // jika FRONT_position > REAR_position, iterasi dari FRONT ke akhir array dan dari awal array ke REAR
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
+            // iterasi dari awal array ke REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
+
